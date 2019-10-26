98385955e40f993494c82aaeb902aa6c95cfe2a1
Nikos Ch Papadopoulos <ncpapadop@gmail.com>
2019-10-26 20:42:11 +0300

    added support for set associative TLB

--- rocket-panel.sh
+++ rocket-panel.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-L1=("32" "32" "64" "64" "128" "128" "256" "256" "512" "512" "512" "1024") 
-L2=("64" "128" "128" "256" "256" "512" "512" "1024" "1024" "1024" "2048" "4096")
-PTWC=("8" "8" "16" "16" "32" "32" "64" "64" "64" "128" "256" "512")
+# Different TLB combinations
+#L1SETS=("32" "32" "64" "64" "128" "128" "256" "256" "512" "512") 
+#L2=("64" "128" "128" "256" "256" "512" "512" "1024" "1024" "1024")
+#PTWC=("8" "8" "16" "16" "32" "32" "64" "64" "64" "128")
+
+L1SETS=("512" "1" "8")
+L1WAYS=("1" "512" "64")
+L2=("0" "0" "0" )
+PTWC=("8" "8" "8")
+
+
+# Timing analysis on the L1SETS TLB
+#L1SETS=("32" "64" "128" "256" "512") 
+#L2=("0" "0" "0" "0" "0")
+#PTWC=("8" "8" "8" "8" "8")
+
+# Timing analysis on the L2 TLB
+#L1SETS=("32" "32" "32" "32" "32")
+#L2=("32" "64" "128" "256" "512") 
+#PTWC=("8" "8" "8" "8" "8")
 
 BASE_DIR=$(pwd)
 ROCKET_DIR=$BASE_DIR/../rocket-chip/src/main/scala/rocket
@@ -10,23 +27,32 @@ HCACHE=$ROCKET_DIR/HellaCache.scala
 RCORE=$ROCKET_DIR/RocketCore.scala
 PTWALK=$ROCKET_DIR/PTW.scala
 
+MAKEFRAG=$BASE_DIR/../common/Makefrag.zcu
 BOOT_BIN=$BASE_DIR/petalinux_proj/images/linux/BOOT.BIN
 GEN_BITSTREAM=$BASE_DIR/../../generated-bitstream
 
-for i in ${!L1[@]} 
+for i in ${!L1SETS[@]} 
 do
-	NL1=${L1[$i]} 
+	NL1SETS=${L1SETS[$i]} 
+	NL1WAYS=${L1WAYS[$i]} 
 	NL2=${L2[$i]} 
 	NPTWC=${PTWC[$i]}
 
-	echo $NL1 $NL2 $NPTWC
-	sed -i "/nTLBEntries/c\    nTLBEntries: Int = $NL1," $HCACHE
+	echo $NL1SETS $NL1WAYS $NL2 $NPTWC
+	sed -i "/nTLBSets/c\    nTLBSets: Int = $NL1SETS," $HCACHE
+	sed -i "/nTLBWays/c\    nTLBWays: Int = $NL1WAYS," $HCACHE
 	sed -i "/nL2TLBEntries/c\  nL2TLBEntries: Int = $NL2," $RCORE
 	sed -i "119s/.*/    val size = $NPTWC/" $PTWALK
 
 	sh generate-bitstream.sh
 	
-	cp $BOOT_BIN $GEN_BITSTREAM/BOOT.BIN_${NL1}_${NL2}_${NPTWC}
+	cp $BOOT_BIN $GEN_BITSTREAM/BOOT.BIN_${NL1SETS}S-${NL1WAYS}W_${NL2}_${NPTWC} 
+	
+	mv zcu102_rocketchip_ZynqConfig zcu102_rocketchip_ZynqConfig__${NL1SETS}S-${NL1WAYS}W_${NL2}_${NPTWC} 
+
 done
+
+# Remove Vivado logs
+rm vivado*
 
 
